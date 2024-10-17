9e6f37eededbda4dcb9b805cf4f9dbbaca1ea7a6
Guilherme Faria <guilherme.ap404@gmail.com>
2024-10-17 14:22:31 -0300

    ajuste de banco de dados

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,24 +1,10 @@
+const {DB_USERNAME, DB_PASS, DB_DATABASE, DB_HOST, DB_PORT, DB_DIALECT}=process.env
+
 module.exports = {
-    "development": {
-        "username": "root",
-        "password": null,
-        "database": "taskmarker",
-        "host": "127.0.0.1",
-        "port": "3306",
-        "dialect": "mysql",
-    },
-    "test": {
-        "username": "root",
-        "password": null,
-        "database": "database_test",
-        "host": "127.0.0.1",
-        "dialect": "mysql"
-    },
-    "production":{
-        "username":process.env.DB_USER,
-        "database":process.env.DB_STORAGE,
-        "password":process.env.DB_PASS,
-        "host":process.env.DB_HOST,
-        "dialect": "postgres",
-    }
+    "username": DB_USERNAME,
+    "password": DB_PASS,
+    "database": DB_DATABASE,
+    "host": DB_HOST,
+    "port": DB_PORT,
+    "dialect": DB_DIALECT,
 };
